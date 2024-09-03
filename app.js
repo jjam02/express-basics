50227cbda33b179a59c1e740ebf8340ef9190c5c
Jonathan Alvarez <jony02@icloud.com>
2024-09-03 13:01:51 -0700

    basic server with 2 routes and test html files

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.listen(port, () => {
+  console.log(`server is running listening on port ${port}`);
+});
