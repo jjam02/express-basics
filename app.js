b8f383a36952ef89fd421f51cfd67bd2a976f653
Jonathan Alvarez <jony02@icloud.com>
2024-09-03 17:42:13 -0700

    added cookies

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const port = 3000;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set("view engine", "pug"); // Replace 'pug' with your chosen engine
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  if (req.cookies.username) {
+    res.render("index", { name: req.cookies.username });
+  } else {
+    res.render("hello");
+  }
+
   console.log("INDEX PAGE");
 });
 
 app.get("/cards", (req, res) => {
-  res.render("card", { prompt: "chungus" });
+  res.render("card", { prompt: "test card" });
 });
 
 app.get("/hello", (req, res) => {
-  res.render("hello");
+  res.render("hello", { name: req.cookies.username });
   console.log("HELLO PAGE");
 });
 
 app.post("/hello", (req, res) => {
-  console.log(req.body.username);
-  res.render("hello", { name: req.body.username });
+  res.cookie("username", req.body.username);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
