cf27102f336674ed62ae7b4ef5696cea6e387d64
Jonathan Alvarez <jony02@icloud.com>
2024-09-04 09:56:20 -0700

    pull the routes out to a seperate file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const port = 3000;
+const routes = require("./routes");
 
 const app = express();
 
@@ -11,34 +12,7 @@ app.use(cookieParser());
 app.set("view engine", "pug"); // Replace 'pug' with your chosen engine
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  if (req.cookies.username) {
-    res.render("index", { name: req.cookies.username });
-  } else {
-    res.render("hello");
-  }
-
-  console.log("INDEX PAGE");
-});
-
-app.get("/cards", (req, res) => {
-  res.render("card", { prompt: "test card" });
-});
-
-app.get("/hello", (req, res) => {
-  res.render("hello", { name: req.cookies.username });
-  console.log("HELLO PAGE");
-});
-
-app.post("/hello", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/");
-});
-
-app.post("/goodbye", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/hello");
-});
+app.use(routes);
 
 app.use((req, res, next) => {
   const err = new Error("Not Found");
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  if (req.cookies.username) {
+    res.render("index", { name: req.cookies.username });
+  } else {
+    res.render("hello");
+  }
+
+  console.log("INDEX PAGE");
+});
+
+router.get("/cards", (req, res) => {
+  res.render("card", { prompt: "test card" });
+});
+
+router.get("/hello", (req, res) => {
+  res.render("hello", { name: req.cookies.username });
+  console.log("HELLO PAGE");
+});
+
+router.post("/hello", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/");
+});
+
+router.post("/goodbye", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/hello");
+});
+
+module.exports = router;
