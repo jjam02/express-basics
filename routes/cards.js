ab6ea3cefd3d2b65a6b35be34567aa89d5471733
Jonathan Alvarez <jony02@icloud.com>
2024-09-04 11:42:58 -0700

    flash card navigation routes set

--- routes/cards.js
+++ routes/cards.js
@@ -8,8 +8,15 @@ router.get("/:id", (req, res) => {
   const { id } = req.params;
   const text = cards[id][side];
   const { hint } = cards[id];
-  const templateData = { text, hint, side, id };
+  const max = cards.length - 1;
+  const templateData = { text, hint, side, id, max };
   res.render("card", templateData);
+});
+
+router.get("/", (req, res) => {
+  const max = cards.length - 1;
+  const cardNum = Math.floor(Math.random() * (max - 0 + 1)) + 0;
+  res.redirect(`/cards/${cardNum}?side=question`);
 });
 
 module.exports = router;
